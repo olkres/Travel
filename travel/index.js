13720cb3b3175246b3396da321b7699476797c1c
olkres <belirium29@gmail.com>
2022-07-17 16:52:28 +0300

    feat: make slider in section destinations

--- travel/index.js
+++ travel/index.js
@@ -1,3 +1,24 @@
+// slider
+let offset = 0;
+const sliderLine = document.querySelector('.destination');
+
+document.querySelector('.radio_1').addEventListener('click', function(){
+    offset = 860;
+    sliderLine.style.left = offset + 'px';
+});
+
+document.querySelector('.radio_2').addEventListener('click', function(){
+    offset = 0;
+    sliderLine.style.left = offset + 'px';
+});
+
+document.querySelector('.radio_3').addEventListener('click', function(){
+    offset = - 860;
+    sliderLine.style.left = offset + 'px';
+});
+
+// burger menu
+
 (function () {
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.nav');
